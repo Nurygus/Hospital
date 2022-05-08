192799b50588923c8982762f04f91552299f4e40
Bega <bega2207.bb@gmail.com>
2022-05-08 22:02:39 +0500

    Made login page

--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "login"
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sign_in/', views.sign_in, name='sign_in'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
 def index(request):
     return render(request, "login/index.html")
+
+def sign_in(request):
+    user = authenticate(username = request.POST['username'], password = request.POST['password'])
+    if user is not None:
+        return HttpResponseRedirect(reverse("doctorsApp:index"))
+    else:
+        return HttpResponseRedirect(reverse("login:index"))
