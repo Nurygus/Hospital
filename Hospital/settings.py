08cebacbfd0c5aaf79d255e9683f2d7575625606
Bega <bega2207.bb@gmail.com>
2022-05-08 20:43:35 +0500

    App login was included into INSTALLED_APPS of project

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'main.apps.MainConfig',
     'doctorsApp.apps.DoctorsAppConfig',
     'patientsApp.apps.PatientsAppConfig',
+    'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
