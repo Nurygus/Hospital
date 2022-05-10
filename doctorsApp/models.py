a6d4a1e09017a1e7cef33d95c59436ef8d31e354
Bega <bega2207.bb@gmail.com>
2022-05-10 18:15:01 +0500

    Models Doctors and Patients included to admin

--- doctorsApp/models.py
+++ doctorsApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Doctors(models.Model):
+    name = models.TextField()
+    speciality_id = models.IntegerField()
+    hospital_id = models.IntegerField()
+    department_id = models.IntegerField()
+    position_at_work = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'doctors'
--- patientsApp/models.py
+++ patientsApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Patients(models.Model):
+    name = models.TextField()
+    date_of_birth = models.DateField()
+    country = models.TextField()
+    city = models.TextField()
+    etrap = models.TextField()
+    hospital_id = models.IntegerField()
+    doctor_id = models.IntegerField()
+    work_address = models.TextField()
+    work = models.TextField()
+    phone = models.TextField()
+    email = models.TextField()
