8b109987b689cb8a32acf8560f58dd6adee517f5
Bega <bega2207.bb@gmail.com>
2022-05-13 23:57:36 +0500

    Message system is worked

--- doctorsApp/views.py
+++ doctorsApp/views.py
@@ -3,7 +3,9 @@ from django.urls import reverse
 from django.shortcuts import render
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
+from doctorsApp.models import Doctor
 from doctorsApp.models import Hospital
 from patientsApp.models import PatientsApplications
 
@@ -17,6 +19,7 @@ def index(request):
     else:
         return HttpResponseRedirect(reverse("login:index"))
 
+@login_required
 def is_doctor(request):
     try:
         request.user.doctor
@@ -25,8 +28,9 @@ def is_doctor(request):
     else: 
         return True
 
+@login_required
 def globalInbox(request):
-    messages = PatientsApplications.objects.raw('SELECT * FROM patientsapp_patientsapplications WHERE hospital_id = null')
+    messages = PatientsApplications.objects.raw('SELECT * FROM patientsapp_patientsapplications WHERE hospital_id is null')
     hospitals = Hospital.objects.all()
 
     context = {
@@ -35,14 +39,45 @@ def globalInbox(request):
     }
     return render(request, "doctorsApp/globalInbox.html", context)
 
+@login_required
 def globalInboxPost(request):
     updateHospitalOfMessage(request)
     return HttpResponseRedirect(reverse("doctorsApp:globalInbox"))
 
+@login_required
 def updateHospitalOfMessage(request):
-    patientApplication = PatientsApplications.objects.get(id = 1)
+    patientApplication = PatientsApplications.objects.get(id = request.POST.get('message-id'))
     patientApplication.hospital_id = request.POST.get('hospital')
     patientApplication.save()
 
+@login_required
 def localInbox(request):
-    return render(request, "doctorsApp/localInbox.html") 
+    messages = PatientsApplications.objects.raw('SELECT * FROM patientsapp_patientsapplications WHERE hospital_id = ' + str(request.user.doctor.hospital_id) + ' AND doctor_id is null')
+    doctors = Doctor.objects.raw('SELECT * FROM doctorsapp_doctor WHERE hospital_id = ' + str(request.user.doctor.hospital_id))
+
+    context = {
+        'messages': messages,
+        'doctors': doctors
+    }
+    return render(request, "doctorsApp/localInbox.html", context) 
+    
+@login_required
+def localInboxPost(request):
+    updateDoctorOfMessage(request)
+    return HttpResponseRedirect(reverse("doctorsApp:localInbox"))
+    
+@login_required
+def updateDoctorOfMessage(request):
+    patientApplication = PatientsApplications.objects.get(id = request.POST.get('message-id'))
+    patientApplication.doctor_id = request.POST.get('doctor')
+    patientApplication.save()
+
+@login_required
+def doctorInbox(request):
+    messages = PatientsApplications.objects.raw('SELECT * FROM patientsapp_patientsapplications WHERE doctor_id = ' + str(request.user.id))
+
+    context = {
+        'messages': messages
+    }
+    return render(request, "doctorsApp/doctorInbox.html", context) 
+
